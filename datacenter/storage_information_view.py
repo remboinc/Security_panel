7d04b30b49f54be21a1a2779541baa1fc0b1593a
remboinc <remboinc@mail.ru>
2023-05-16 01:36:26 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,23 +5,26 @@ from django.shortcuts import render
 def storage_information_view(request):
     not_leaved = Visit.objects.filter(leaved_at=None)
 
-    for visit in not_leaved:
-        entered_at = Visit.objects.filter(leaved_at=None)[0].entered_at
-        visit_duration = visit.get_duration(visit)
-        format_duration = visit.format_duration(visit_duration)
-        is_strange = visit.is_visit_long(visit, minutes=60)
-        names = ", ".join(visit)
+    non_closed_visits = []
 
-        non_closed_visits = [
-            {
-                'who_entered': names,
-                'entered_at': entered_at,
-                'duration': format_duration,
-                'is_strange': is_strange,
-            }
-        ]
-        context = {
-            'non_closed_visits': non_closed_visits,
+    for visit in not_leaved:
+        entered_at = visit.entered_at
+        visit_duration = visit.get_duration()
+        format_duration = visit.format_duration(visit_duration)
+        is_strange = visit.is_visit_long(minutes=60)
+        owner_name = visit.passcard.owner_name
+
+        visit_info = {
+            'who_entered': owner_name,
+            'entered_at': entered_at,
+            'duration': format_duration,
+            'is_strange': is_strange,
         }
-        non_closed_visits.append(visit.passcard.owner_name)
-        return render(request, 'storage_information.html', context)
+
+        non_closed_visits.append(visit_info)
+
+    context = {
+        'non_closed_visits': non_closed_visits,
+    }
+
+    return render(request, 'storage_information.html', context)
