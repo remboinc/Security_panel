859027383c98139af8eced9431e029bc2ef64687
remboinc <remboinc@mail.ru>
2023-05-22 23:25:51 +0300

    Delete unuseful code

--- project/settings.py
+++ project/settings.py
@@ -2,28 +2,21 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-db_engine = os.getenv('DB_ENGINE')
-db_host = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT')
-db_name = os.getenv('DB_NAME')
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-app_secret_key = os.getenv('APP_SECRET_KEY')
 
 DATABASES = {
     'default': {
-        'ENGINE': db_engine,
-        'HOST': db_host,
-        'PORT': db_port,
-        'NAME': db_name,
-        'USER': db_user,
-        'PASSWORD': db_password,
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = app_secret_key
+SECRET_KEY = os.getenv('APP_SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG') == 'False'
 
